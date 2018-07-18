ef1c344be5ebf5fc495a9b6008edb1d2ed7cbd92
Hesham Massoud <heshamhamdymassoud@gmail.com>
2018-07-18 22:40:42 +0200

    #1731: Add custom reference expansion for ShoppingListExpansionModel.

--- commercetools-models/src/main/java/io/sphere/sdk/shoppinglists/expansion/ShoppingListExpansionModel.java
+++ commercetools-models/src/main/java/io/sphere/sdk/shoppinglists/expansion/ShoppingListExpansionModel.java
@@ -2,12 +2,15 @@ package io.sphere.sdk.shoppinglists.expansion;
 
 import io.sphere.sdk.customers.expansion.CustomerExpansionModel;
 import io.sphere.sdk.expansion.ExpansionPathContainer;
+import io.sphere.sdk.products.expansion.CustomExpansionModel;
 import io.sphere.sdk.shoppinglists.ShoppingList;
 
 public interface ShoppingListExpansionModel<T> extends ExpansionPathContainer<T> {
     CustomerExpansionModel<T> customer();
 
     LineItemExpansionModel<T> lineItems();
+
+    CustomExpansionModel<T> custom();
 
     static ShoppingListExpansionModel<ShoppingList> of() {
         return new ShoppingListExpansionModelImpl<>();
--- commercetools-models/src/main/java/io/sphere/sdk/shoppinglists/expansion/ShoppingListExpansionModelImpl.java
+++ commercetools-models/src/main/java/io/sphere/sdk/shoppinglists/expansion/ShoppingListExpansionModelImpl.java
@@ -2,6 +2,8 @@ package io.sphere.sdk.shoppinglists.expansion;
 
 import io.sphere.sdk.customers.expansion.CustomerExpansionModel;
 import io.sphere.sdk.expansion.ExpansionModelImpl;
+import io.sphere.sdk.products.expansion.CustomExpansionModel;
+import io.sphere.sdk.products.expansion.CustomExpansionModelImpl;
 
 import java.util.List;
 
@@ -23,4 +25,11 @@ final class ShoppingListExpansionModelImpl<T> extends ExpansionModelImpl<T> impl
     public LineItemExpansionModel<T> lineItems() {
         return LineItemExpansionModel.of(buildPathExpression(), "lineItems[*]");
     }
+
+    @Override
+    public CustomExpansionModel<T> custom() {
+        return new CustomExpansionModelImpl<>(pathExpression(), "custom");
+    }
+
+    //TODO: TextLineItem is missing!
 }
