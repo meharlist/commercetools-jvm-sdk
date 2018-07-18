69b282ed9316037c9d8b4cdb1d09c9d42609d9de
Hesham Massoud <heshamhamdymassoud@gmail.com>
2018-07-18 22:38:32 +0200

    #1731: Add custom reference expansion for InventoryEntryExpansionModel.

--- commercetools-models/src/main/java/io/sphere/sdk/inventory/expansion/InventoryEntryExpansionModel.java
+++ commercetools-models/src/main/java/io/sphere/sdk/inventory/expansion/InventoryEntryExpansionModel.java
@@ -2,9 +2,12 @@ package io.sphere.sdk.inventory.expansion;
 
 import io.sphere.sdk.channels.expansion.ChannelExpansionModel;
 import io.sphere.sdk.inventory.InventoryEntry;
+import io.sphere.sdk.products.expansion.CustomExpansionModel;
 
 public interface InventoryEntryExpansionModel<T> {
     ChannelExpansionModel<T> supplyChannel();
+
+    CustomExpansionModel<T> custom();
 
     static InventoryEntryExpansionModel<InventoryEntry> of() {
         return new InventoryEntryExpansionModelImpl<>();
--- commercetools-models/src/main/java/io/sphere/sdk/inventory/expansion/InventoryEntryExpansionModelImpl.java
+++ commercetools-models/src/main/java/io/sphere/sdk/inventory/expansion/InventoryEntryExpansionModelImpl.java
@@ -2,6 +2,8 @@ package io.sphere.sdk.inventory.expansion;
 
 import io.sphere.sdk.channels.expansion.ChannelExpansionModel;
 import io.sphere.sdk.expansion.ExpansionModelImpl;
+import io.sphere.sdk.products.expansion.CustomExpansionModel;
+import io.sphere.sdk.products.expansion.CustomExpansionModelImpl;
 
 final class InventoryEntryExpansionModelImpl<T> extends ExpansionModelImpl<T> implements InventoryEntryExpansionModel<T> {
 
@@ -11,5 +13,10 @@ final class InventoryEntryExpansionModelImpl<T> extends ExpansionModelImpl<T> im
     @Override
     public ChannelExpansionModel<T> supplyChannel() {
         return ChannelExpansionModel.of(buildPathExpression(), "supplyChannel");
+    }
+
+    @Override
+    public CustomExpansionModel<T> custom() {
+        return new CustomExpansionModelImpl<>(pathExpression(), "custom");
     }
 }
