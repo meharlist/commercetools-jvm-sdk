b4088d64da55677eeac54124716742e438566613
Hesham Massoud <heshamhamdymassoud@gmail.com>
2018-07-18 21:50:03 +0200

    #1731: Make TypeExpansionModelImpl public.

--- commercetools-models/src/main/java/io/sphere/sdk/types/expansion/TypeExpansionModelImpl.java
+++ commercetools-models/src/main/java/io/sphere/sdk/types/expansion/TypeExpansionModelImpl.java
@@ -2,7 +2,14 @@ package io.sphere.sdk.types.expansion;
 
 import io.sphere.sdk.expansion.ExpansionModelImpl;
 
-final class TypeExpansionModelImpl<T> extends ExpansionModelImpl<T> implements TypeExpansionModel<T> {
+import java.util.List;
+
+public final class TypeExpansionModelImpl<T> extends ExpansionModelImpl<T> implements TypeExpansionModel<T> {
+
+    public TypeExpansionModelImpl(final List<String> parentPath, final String path) {
+        super(parentPath, path);
+    }
+
     TypeExpansionModelImpl(final String parentPath, final String path) {
         super(parentPath, path);
     }
